5d4fe021c69866f85a009ca11ffd81cd629be9eb
filippogustavo <filippogustavo@gmail.com>
2026-05-04 12:03:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ let searchRelSrvQuery = '';
 // 2. AUTO-LOGOUT (SEGURANÇA POR INATIVIDADE)
 // ==========================================
 let inactivityTimer;
-const TEMPO_LIMITE_MINUTOS = 5; 
+const TEMPO_LIMITE_MINUTOS = 10; 
 const INACTIVITY_TIME_MS = TEMPO_LIMITE_MINUTOS * 60 * 1000; 
 
 function resetInactivityTimer() {
